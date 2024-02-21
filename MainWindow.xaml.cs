98c41a4744f8166a5f0e367bafd4836873749f12
uuupppsss <ulya.ch.06@mail.ru>
2024-02-21 01:15:37 -0400

    oaoaoaoaoaoaoao

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http.Json;
 using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -13,20 +17,10 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace spooky_scary_skeletons
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-    }
-
-     public class TasckModel
+    public class TaskModel
     {
         public string Description { get; set; }
         public DateTime CreateDate { get; set; }
@@ -34,4 +28,40 @@ namespace spooky_scary_skeletons
         public string Status { get; set; }
         public DateTime Completedate { get; set; }
     }
+
+    public class TaskManager
+    {
+        private List<TaskModel> tasks;
+        public TaskManager() 
+        {
+            string json = File.ReadAllText("tasks.json"); //загрузка задачи
+            tasks = JsonSerializer.Deserialize<List<TaskModel>>(json);
+
+
+        }
+        public void SaveTasks() //сохранение задач 
+        {
+            string json= JsonSerializer.Serialize(tasks);
+            File.WriteAllText("tasks.json", json);
+        }
+   
+    }
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private TaskManager taskManager;
+
+        public MainWindow()
+        {
+            taskManager = new TaskManager();
+            InitializeComponent();
+
+        }
+
+       
+    }
+
 }
